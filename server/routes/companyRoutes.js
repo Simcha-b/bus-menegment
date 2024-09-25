ec2dc57b3e2872efeab023e26173b34bc405c0a0
Simcha-b <b5860344@gmail.com>
2024-09-26 00:31:50 +0300

    add routes

---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,16 @@
+const router = express.Router();
+import {
+    getCompanies,
+    getCompany,
+    insertCompany,
+    updateCompany,
+    deleteCompany,
+} from "../controllers/companyControllers.js";
+
+router.get("/", getCompanies); 
+router.get("/:id", getCompany);
+router.post("/", insertCompany);
+router.put("/:id", updateCompany);
+router.delete("/:id", deleteCompany);
+
+export default router;
