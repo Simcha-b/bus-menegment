4128df5defd4a2a0795bd2ec51a2d20c6b632202
Simcha-b <b5860344@gmail.com>
2024-10-29 12:45:15 +0200

    add home page

---
+++ client/src/components/OrderTable.jsx
@@ -0,0 +1,81 @@
+import { Table, ConfigProvider } from "antd";
+import heIL from "antd/lib/locale/he_IL";
+import moment from "moment";
+  
+function OrderTable(props) {
+  const data = props.data;
+  const names = data.map((item) => {
+    return {
+      text: item.institution_name,
+      value: item.institution_name,
+    };
+  });
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "reservation_id",
+      key: "reservation_id",
+    },
+    {
+      title: "תאריך",
+      dataIndex: "reservation_date",
+      key: "reservation_date",
+      sorter: (a, b) =>
+        moment(a.reservation_date, "YYYY-MM-DD").unix() -
+        moment(b.reservation_date, "YYYY-MM-DD").unix(),
+      render: (text) => moment(text).format("DD/MM/YYYY"), // מציג את התאריך בפורמט יום/חודש/שנה
+    },
+    {
+      title: "שם",
+      dataIndex: "institution_name",
+      key: "institution_name",
+      sorter: (a, b) => a.institution_name.localeCompare(b.institution_name),
+      filters: names,
+      filterMode: "tree",
+      filterSearch: true,
+      onFilter: (value, record) => record.name.startsWith(value),
+    },
+    {
+      title: "שעת התחלה",
+      dataIndex: "start_time",
+      key: "start_time",
+    },
+    {
+      title: "שעת סיום",
+      dataIndex: "end_time",
+      key: "end_time",
+    },
+    {
+      title: "כמות אוטבוסים",
+      dataIndex: "bus_quantity",
+      key: "bus_quantity",
+      width: "5%",
+    },
+    {
+      title: "פרטי נסיעה",
+      dataIndex: "trip_details",
+      key: "trip_details",
+    },
+    {
+      title: "חברת אוטובוסים",
+      dataIndex: "company_name",
+      key: "company_name",
+      sorter: (a, b) => a.company_name.localeCompare(b.company_name),
+    },
+    {
+      title: "סטטוס",
+      dataIndex: "status",
+      key: "status",
+      sorter: (a, b) => a.status.localeCompare(b.status),
+    },
+  ];
+
+  return (
+    <ConfigProvider direction="rtl" locale={heIL}>
+      <Table columns={columns} dataSource={data} />
+    </ConfigProvider>
+  );
+}
+
+export default OrderTable;
--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -4,10 +4,8 @@ import { useQuery } from "@tanstack/react-query";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { getOrders } from "../services/ordersService";
-import { Table } from "../components/Table";
 
-import AdvancedHebrewTable from "../components/AdvancedHebrewTable";
-import { style } from "@mui/system";
+import OrsersTable from "../components/OrderTable";
 function Orders() {
   const navigate = useNavigate();
   const { isPending, error, data } = useQuery({
@@ -21,12 +19,23 @@ function Orders() {
     return <div>Error</div>;
   }
 
-  return ( 
+  return (
     <>
-      <h1 style={{ textAlign: "center" , margin: "20px", fontSize: "30px"}}>orders</h1>
-      <Button sx={{margin: "20px", alignItems: "center"}} variant="contained" color="success" onClick={() => {navigate("/orders/new")}}> הזמנה חדשה</Button>
-      {/* <AdvancedHebrewTable data={data} /> */}
-      <Table data={data} />
+      <h1 style={{ textAlign: "center", margin: "20px", fontSize: "30px" }}>
+        orders
+      </h1>
+      <Button
+        sx={{ margin: "20px", alignItems: "center" }}
+        variant="contained"
+        color="success"
+        onClick={() => {
+          navigate("/orders/new");
+        }}
+      >
+        {" "}
+        הזמנה חדשה
+      </Button>
+      <OrsersTable data={data} />
     </>
   );
 }
