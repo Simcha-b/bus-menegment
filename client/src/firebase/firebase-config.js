d07923ad7d4bf0d565c6c3898c258fb719d4dad4
Simcha Berkovitz <151738331+Simcha-b@users.noreply.github.com>
2024-11-19 23:50:11 +0200

    fixed a  bug

---
+++ client/src/firebase/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+
+
+const firebaseConfig = {
+  apiKey: `${apiKey}`,
+  authDomain: "bus-management-1c3d2.firebaseapp.com",
+  projectId: "bus-management-1c3d2",
+  storageBucket: "bus-management-1c3d2.appspot.com",
+  messagingSenderId: "671035929507",
+  appId: "1:671035929507:web:abbd3b3d57c693c912a016",
+};
+
+const app = initializeApp(firebaseConfig, {
+  popupRedirectResolver: undefined
+});
+
+export default app;
